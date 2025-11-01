ab1e8a6642572e9f2cb98e4872afcd4b019b9a40
keylorjc1 <106986264+keylorjc1@users.noreply.github.com>
2025-11-01 12:04:58 -0600

    validaciones

--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -1,8 +1,18 @@
 import {Router} from 'express'
+import {body} from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController'
 
 const router = Router()
 
+router.post('/', 
+    body('projectName')
+    .notEmpty().withMessage('The name of the Project is required'),
+    body('clientName')
+    .notEmpty().withMessage('The name of the Client is required'),
+        body('description')
+    .notEmpty().withMessage('The Description is required'),
+    ProjectController.createProject
+)
 router.get('/',ProjectController.getAllProjects)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ dotenv.config()
 connectDB()
 const app = express()
 
+app.use(express.json())
+
 //Routes
 app.use('/api/projects', projectRoutes )
 
